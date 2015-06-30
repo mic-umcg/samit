7da0a8780842c055a7f8d992d79f89ae4bbc5a09
David Vállez Garcia <d.vallez-garcia@umcg.nl>
2015-06-30 16:25:02 +0200

    Update of samit_reset
    Update of the function to solve issues related with how VINCI and PMOD
    handle NIfTI files

--- samit_reset.m
+++ samit_reset.m
@@ -34,8 +34,9 @@ function samit_reset(files,d)
 %
 
 % VINCI saves the images in Ragiological convention
+% and the information in vol.mat is not properly stored
 
-%   Version: 14.12.02 (02 December 2014)
+%   Version: 15.06.23 (23 June 2015)
 %   Author:  David Vállez Garcia
 %   Email:   samit@umcg.nl
 
@@ -55,47 +56,71 @@ end
 %% Select image
 if ~exist('files','var')
     [files, sts] = spm_select(Inf,'image','Select images...','', pwd);
-    if sts == 0
+    if ~sts
         display('Operation cancelled: No files were selected');
         return
-    end
-    files = deblank(files);
+    else
+        files = cellstr(files);
+        %files = deblank(files);
+    end    
 end
 
-for f = 1:size(files)
+for i = 1:numel(files)
     
-    vol = nifti(files(f,:));     % Working image
-    
-    % Check orientation (extracted from spm_orientations)
-    M = vol.mat;
-    [U,~,V] = svd(M(1:3,1:3));
-    M   = U*V';
-    lab = 'LRPAIS';
-    m   = [1 -1  0  0  0  0
-        0  0  1 -1  0  0
-        0  0  0  0  1 -1];
-    dp = M\m;
-    c  = '   ';
-    for j=1:3,
-        [~,ind] = max(dp(j,:));
-        c(j)         = lab(ind);
-    end
-    
-    % Reset orientation
-    
-    if det(M)>0,
-        % 'Right' or 'LPI'
-        M = vol.mat0;
-        M(1,:) = M(1,:) * -1;
-    else
-        % 'Left' or 'RPI'
-        M = vol.mat0;
-    end
-    
-    spm_get_space(files(f,:),M);      
-    
+% Extracted from spm_image('resetorient')
+        V    = spm_vol(files{i});
+        M    = V.mat;
+
+%% VINCI NIfTI Fix (last version tested 4.46):
+% the matrix are not properly stored in the header
+% this fix is needed to avoid wrong orientation and
+% voxel size
+% data is transformed from radiological orientation to neurological
+
+        % Default "wrong" matrix used by VINCI
+        M_vinci = [2 0 0 -2; 0 2 0 -2; 0 0 2 -2; 0 0 0 1];
+        
+        if isequal(M, M_vinci) && ~isequal(M, V.private.mat0)
+            M = V.private.mat0;
+            %M(1) = M(1) * -1;
+            Y = spm_read_vols(V);
+            Y = flip(Y,1);
+            spm_write_vol(V,Y);
+
+        end
+        
+% End VINCI Fix
+
+%% PMOD NIfTI Fix
+%   PMOD saves the files with anterior/posterior flip, as compared with SPM
+        M_pmod = diag([1 -1 1]);
+        [A, ~ , B] = svd(M(1:3,1:3));
+        Pmod = A*B';
+        if isequal(M_pmod, Pmod)
+            M(2,:) = M(2,:) * -1;
+            Y = spm_read_vols(V);
+            Y = flip(Y, 2); % Flip data A/P
+            spm_write_vol(V,Y);
+        end
+        
+
+%% Reset
+        vox  = sqrt(sum(M(1:3,1:3).^2));
+        if det(M(1:3,1:3))<0
+            vox(1) = -vox(1);
+        end
+        orig = (V.dim(1:3)+1)/2;
+        off  = -vox.*orig;
+        M    = [vox(1) 0      0      off(1)
+                0      vox(2) 0      off(2)
+                0      0      vox(3) off(3)
+                0      0      0      1];
+        spm_get_space(files{i},M);
+
+%
+
     if d ~= false
-        display(['Image corrected: ', spm_file(files(f,:),'filename')]);
+        display(['Image corrected: ', spm_file(files{i},'filename')]);
     end
     
 end
